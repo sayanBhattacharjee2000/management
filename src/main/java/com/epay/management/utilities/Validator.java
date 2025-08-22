04636e726f4c6a0a3f1a8610c84a049731d0526a
cristalfield <subhajitbhattacharya1996@gmail.com>
2025-08-22 12:28:32 +0530

    validate email code

--- src/main/java/com/epay/management/utilities/Validator.java
+++ src/main/java/com/epay/management/utilities/Validator.java
@@ -1,10 +1,12 @@
 package com.epay.management.utilities;
 
 import com.epay.management.entity.EmployeeEntity;
+import org.springframework.stereotype.Component;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Component
 public class Validator {
     public boolean isValidEmail(EmployeeEntity employee) {
         String regex = "^[A-Za-z0-9._%+-]+@gmail\\.com$";
