4f9b41bbfe866ef5dff8decf127ece3c3287985a
cristalfield <subhajitbhattacharya1996@gmail.com>
2025-10-12 13:26:39 +0530

    added controller test cases

--- src/test/java/com/epay/management/controller/EmployeeControllerTest.java
+++ src/test/java/com/epay/management/controller/EmployeeControllerTest.java
@@ -1,5 +1,6 @@
 package com.epay.management.controller;
 
+import com.epay.management.entity.EmployeeEntity;
 import com.epay.management.service.EmployeeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(EmployeeController.class)
 class EmployeeControllerTest {
@@ -21,6 +22,7 @@ class EmployeeControllerTest {
 
     @MockitoBean
     private EmployeeService employeeService;
+
 
     @DisplayName("Verify that when user visit employee site, he should get navigated to welcome " +
             "page")
@@ -39,7 +41,85 @@ class EmployeeControllerTest {
         mockMvc.perform(get("/employee/list"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("employee-list"));
+        Mockito.verify(employeeService, Mockito.times(1)).getList();
+    }
 
-        Mockito.verify(employeeService,Mockito.times(1)).getList();
+    @Test
+    void testCreateEmployee_WhenUserCreatesAccount_AccountShouldGetCreated() throws Exception {
+        mockMvc.perform(post("/employee/save"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/employee/list"));
+
+        Mockito.verify(employeeService,Mockito.times(1)).saveEmployee(Mockito.any(EmployeeEntity.class));
+    }
+
+    @Test
+    void testCreateEmployee_WhenUserCreatesAccount_ThrowException() throws Exception {
+
+        Mockito.doThrow(new IllegalArgumentException("Invalid data"))
+                .when(employeeService).saveEmployee(Mockito.any(EmployeeEntity.class));
+
+        mockMvc.perform(post("/employee/save"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("employee-form"))
+                .andExpect(model().attributeExists("errorMessage"))
+                .andExpect(model().attributeExists("employee"));
+
+        Mockito.verify(employeeService,Mockito.times(1)).saveEmployee(Mockito.any(EmployeeEntity.class));
+    }
+
+    @Test
+    void testUpdateEmployee_WhenUserUpdatesAccount_AccountShouldGetUpdated() throws Exception {
+        EmployeeEntity employee = employee();
+
+        mockMvc.perform(post("/employee/save")
+                        .flashAttr("employee",employee))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/employee/list"));
+
+        Mockito.verify(employeeService,Mockito.times(1)).updateEmployee(employee.getId(), employee);
+        Mockito.verify(employeeService,Mockito.times(0)).saveEmployee(employee);
+    }
+
+    @Test
+    void testUpdateEmployee_WhenUserUpdatesAccount_ThrowException() throws Exception {
+        EmployeeEntity employee = employee();
+
+        Mockito.doThrow(new IllegalArgumentException("Invalid data"))
+                .when(employeeService).updateEmployee(Mockito.anyLong(),
+                        Mockito.any(EmployeeEntity.class));
+
+        mockMvc.perform(post("/employee/save")
+                        .flashAttr("employee", employee))
+                .andExpect(status().isOk())
+                .andExpect(view().name("employee-form"))
+                .andExpect(model().attributeExists("errorMessage"))
+                .andExpect(model().attributeExists("employee"));
+
+        Mockito.verify(employeeService,Mockito.times(1)).updateEmployee(employee.getId(), employee);
+    }
+
+    @DisplayName("Verify that when user deletes an employee, it should call service and redirect")
+    @Test
+    void testDeleteEmployee_WhenUserClickDelete_ShouldRemoveEmployee() throws Exception {
+        EmployeeEntity employee = employee();
+
+        Mockito.when(employeeService.getEmployeeById(1L)).thenReturn(employee);
+
+        mockMvc.perform(get("/employee/delete/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/employee/list"));
+
+        Mockito.verify(employeeService, Mockito.times(1)).deleteEmployee(1L);
+    }
+
+    private EmployeeEntity employee(){
+        EmployeeEntity employee = new EmployeeEntity();
+        employee.setId(1L);
+        employee.setFirstName("Sayan");
+        employee.setLastName("Bhattacharjee");
+        employee.setAge(26);
+        employee.setEmail("IT");
+        return employee;
     }
 }
--- src/test/java/com/epay/management/service/EmployeeServiceTest.java
+++ src/test/java/com/epay/management/service/EmployeeServiceTest.java
@@ -1,7 +1,36 @@
 package com.epay.management.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.epay.management.entity.EmployeeEntity;
+import com.epay.management.repository.EmployeeRepository;
+import com.epay.management.utilities.Validator;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Optional;
 
 class EmployeeServiceTest {
 
+    @Mock
+    EmployeeRepository employeeRepository = Mockito.mock(EmployeeRepository.class);
+
+    @Mock
+    Validator validator;
+
+    @Test
+    void testGetEmployeeById_ValidEmployeeIsPresent_ShouldReturnEmployee(){
+        EmployeeEntity employee = employee();
+        Mockito.when(employeeRepository.findById(1L)).thenReturn(Optional.of(employee));
+        Mockito.verify(employeeRepository, Mockito.times(1)).findById(1L);
+    }
+
+    private EmployeeEntity employee(){
+        EmployeeEntity employee = new EmployeeEntity();
+        employee.setId(1L);
+        employee.setFirstName("Sayan");
+        employee.setLastName("Bhattacharjee");
+        employee.setAge(26);
+        employee.setEmail("IT");
+        return employee;
+    }
 }
